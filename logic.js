0f70938d80497f433d6b411b60de79e548026b0c
Jordan Boggs <boggsjp@gmail.com>
2017-12-16 10:49:13 -0700

    initialize firebase

---
+++ logic.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBPCp1w9GM4eJULb2QZgU1vekPSBzxqrVY",
+    authDomain: "class-work-saturday-171216.firebaseapp.com",
+    databaseURL: "https://class-work-saturday-171216.firebaseio.com",
+    projectId: "class-work-saturday-171216",
+    storageBucket: "class-work-saturday-171216.appspot.com",
+    messagingSenderId: "995194489512"
+  };
+  firebase.initializeApp(config);

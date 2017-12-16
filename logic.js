159d865affd15d72be97aedd4ef280e93fcf35f5
Jordan Boggs <boggsjp@gmail.com>
2017-12-16 10:54:21 -0700

    copying code from francis

--- logic.js
+++ logic.js
@@ -1,10 +1,23 @@
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyBPCp1w9GM4eJULb2QZgU1vekPSBzxqrVY",
-    authDomain: "class-work-saturday-171216.firebaseapp.com",
-    databaseURL: "https://class-work-saturday-171216.firebaseio.com",
-    projectId: "class-work-saturday-171216",
-    storageBucket: "class-work-saturday-171216.appspot.com",
-    messagingSenderId: "995194489512"
-  };
-  firebase.initializeApp(config);
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBPCp1w9GM4eJULb2QZgU1vekPSBzxqrVY",
+  authDomain: "class-work-saturday-171216.firebaseapp.com",
+  databaseURL: "https://class-work-saturday-171216.firebaseio.com",
+  projectId: "class-work-saturday-171216",
+  storageBucket: "class-work-saturday-171216.appspot.com",
+  messagingSenderId: "995194489512"
+};
+firebase.initializeApp(config);
+
+var ref = firebase.database().ref();
+
+var name = "";
+var email = "";
+var age = 0;
+var comment = "";
+
+$("#add-user").on("click", function() {
+  event.preventDefault();
+
+  
+});
